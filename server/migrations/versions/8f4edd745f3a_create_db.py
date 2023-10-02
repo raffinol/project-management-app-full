76591378c3a34ff5527a8a9b0d0f48aeb6e5aede
Rafael Finol <rafaelfinol88@gmail.com>
2023-10-01 22:25:58 -0400

    create DB

---
+++ server/migrations/versions/8f4edd745f3a_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: 8f4edd745f3a
+Revises: 
+Create Date: 2023-10-01 20:33:38.970620
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8f4edd745f3a'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
